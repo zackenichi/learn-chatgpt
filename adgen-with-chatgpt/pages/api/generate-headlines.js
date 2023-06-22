d5ad1b718d341faaea1c3a68033c6cbfed40e412
ZacKenichi <thekenichi26@gmail.com>
2023-06-22 21:10:55 +0800

    [feature][zac] content generation
    - test create headline from static prompt

--- adgen-with-chatgpt/pages/api/generate-headlines.js
+++ adgen-with-chatgpt/pages/api/generate-headlines.js
@@ -1,6 +1,4 @@
 import { Configuration, OpenAIApi } from 'openai';
-import axios from 'axios';
-import cheerio from 'cheerio';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,105 +6,51 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-export default async function handler(req, res) {
-  if (!configuration.apiKey) {
-    return res.status(500).json({
-      error: {
-        message: 'Missing API key',
-      },
-    });
-  }
-
-  try {
-    const siteData = await getSiteContent(req.query.baseUrl); // Assuming the site data is fetched from the query parameter 'baseUrl'
-
-    // console.dir(siteData, { depth: null });
-
-    if (siteData.length === 0) {
-      return res.status(400).json({
-        error: {
-          message: 'Site data not found',
-        },
-      });
-    }
-
-    const adHeadline = await generateAdHeadline(siteData);
-
-    // console.log(adHeadline);
-    // const adBody = await generateAdBody(siteData);
-
-    return res.status(200).json({
-      adHeadline,
-      //   adBody,
-    });
-  } catch (error) {
-    console.error('Error:', error.message);
-    return res.status(500).json({
-      error: {
-        message: 'An error occurred during ad generation',
-      },
-    });
-  }
-}
-
-async function generateAdHeadline(siteData) {
-  const prompt =
-    'Create an attention-grabbing headline using the content provided:';
-  const contentText = siteData.map((section) => section.content).join(' ');
-
-  const response = await openai.createCompletion({
-    model: 'text-davinci-004',
-    prompt: `${prompt}\n\n${contentText}`,
+async function generateContent(contentType, contentCount) {
+  const contentResponse = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: generatePrompt(contentType, contentCount),
     temperature: 0.6,
   });
 
-  console.log(response);
+  console.log('choices object', contentResponse.data.choices);
+
+  let text = contentResponse.data.choices[0].text.trim();
+  text = text.replace(/^["'\s]+|["'\s]+$/g, '');
+
+  return text;
 }
 
-// async function generateAdBody(siteData) {
-//   const prompt = 'Write the body text using the content provided:';
-//   const contentText = siteData.map((section) => section.content).join('\n\n');
+// generate for ad headline or body
+// separate function as this will have more logic soon
+function generatePrompt(contentType, count) {
+  let prompt = '';
+  if (contentType === 'headline') {
+    prompt = `Create an attention ${count} grabbing headlines at 30 characters max`;
+  } else {
+    prompt = `Create an attention grabbing ${count} body text with betweem 30 to 90 characters`;
+  }
 
-//   const response = await openai.createCompletion({
-//     model: 'text-davinci-004',
-//     prompt: `${prompt}\n\n${contentText}`,
-//     temperature: 0.6,
-//   });
+  return prompt;
+}
 
-//   if (response.choices && response.choices.length > 0) {
-//     return response.choices[0].text.trim();
-//   } else {
-//     throw new Error('No body text generated');
-//   }
-// }
-
-async function getSiteContent(url) {
+export default async function handler(req, res) {
   try {
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
+    const contentCount = 3;
 
-    const sections = [];
+    const headline = await generateContent('headline', contentCount);
+    const bodyText = await generateContent('body', contentCount);
 
-    $('section').each((index, element) => {
-      const section = {};
-      section.id = `section-${index + 1}`;
-      section.content = [];
-
-      $(element)
-        .find('h1, h2, h3, h4, p')
-        .each((_index, el) => {
-          const elementType = $(el).prop('tagName').toLowerCase();
-          const content = $(el).text().trim().replace(/\s+/g, ' ');
-
-          section.content.push({ type: elementType, content });
-        });
-
-      sections.push(section);
+    return res.status(200).json({
+      headline,
+      bodyText,
     });
-
-    return sections;
   } catch (error) {
     console.error('Error:', error.message);
-    throw error;
+    return res.status(500).json({
+      error: {
+        message: 'An error occurred during headline generation',
+      },
+    });
   }
 }
--- adgen-with-chatgpt/pages/index.js
+++ adgen-with-chatgpt/pages/index.js
@@ -12,32 +12,31 @@ export default function HomePage() {
   const [isLoading, setIsLoading] = useState(false);
   const [showError, setShowError] = useState(false);
 
-  const handleTest = async () => {
-    const animalInput = urlRef.current.value;
-    setIsLoading(true);
+  // const handleTest = async () => {
+  //   const animalInput = urlRef.current.value;
+  //   setIsLoading(true);
 
-    try {
-      const response = await axios.post('/api/test-chatgpt', {
-        animal: animalInput,
-      });
+  //   try {
+  //     const response = await axios.post('/api/test-chatgpt', {
+  //       animal: animalInput,
+  //     });
 
-      const data = response.data;
-      if (response.status !== 200) {
-        throw (
-          data.error ||
-          new Error(`Request failed with status ${response.status}`)
-        );
-      }
+  //     const data = response.data;
+  //     if (response.status !== 200) {
+  //       throw (
+  //         data.error ||
+  //         new Error(`Request failed with status ${response.status}`)
+  //       );
+  //     }
 
-      setIsLoading(false);
-      console.dir(data.result, { depth: null });
-    } catch (error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
-      setIsLoading(false);
-    }
-  };
+  //     setIsLoading(false);
+  //     console.dir(data.result, { depth: null });
+  //   } catch (error) {
+  //     console.error(error);
+  //     alert(error.message);
+  //     setIsLoading(false);
+  //   }
+  // };
 
   const handleScrape = async () => {
     const urlInput = urlRef.current.value;
@@ -46,23 +45,26 @@ export default function HomePage() {
     if (isValidUrl(urlInput)) {
       try {
         // Make sure input is in proper format
-        // const encodedUrl = encodeURIComponent(addHttpToUrl(urlInput));
+        const encodedUrl = encodeURIComponent(addHttpToUrl(urlInput));
+
+        // Separate scraping and chatGPT request
+        const scrapeResponse = await axios.post(
+          `/api/get-page-text?baseUrl=${encodedUrl}`
+        );
+        const siteData = scrapeResponse.data;
+        setSiteData(siteData);
+        // console.dir(siteData, { depth: null });
 
         // Fetch ad headline and body using the API route
-        // const scrapeResponse = await axios.get(
-        //   `/api/generate-headlines?baseUrl=${encodedUrl}`
-        // );
+        const headlineResponse = await axios.post('/api/generate-headlines', {
+          siteData,
+        });
 
-        // const { adHeadline, adBody } = scrapeResponse.data;
+        // test - logging for now
 
-        // const { adHeadline } = scrapeResponse.data;
+        console.log(headlineResponse.data);
 
-        // setSiteData(scrapeResponse.data);
         setIsLoading(false);
-
-        // Do something with the generated ad headline and body
-        // console.log('Ad Headline:', adHeadline);
-        // console.log('Ad Body:', adBody);
       } catch (error) {
         console.error('Error:', error.message);
         setIsLoading(false);
@@ -88,8 +90,8 @@ export default function HomePage() {
         <Grid item xs={12}>
           <Search
             urlRef={urlRef}
-            // onScrape={handleScrape}
-            onScrape={handleTest}
+            onScrape={handleScrape}
+            // onScrape={handleTest}
           />
         </Grid>
         {siteData.length > 0 && (
