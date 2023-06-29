c1cc508bd42796bf75b42ee315a9352487947605
zackenichi <thekenichi26@gmail.com>
2023-06-29 23:36:31 +0800

    First version
    - Prompt engineering needs work
    - scrapes data from given url
    - only gets text from homepage due to the chatgpt token limit
    - generates 3 headlines and copies from scraped text

---
+++ node-openai/generate-ad.js
@@ -0,0 +1,113 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { Configuration, OpenAIApi } from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// set number of choices to get here
+const numberOfChoices = 3;
+
+const getHtml = async (url) => {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const divs = [];
+    const extractedContent = new Set(); // Track extracted content to avoid duplicates
+
+    $('div:not(footer *)').each((index, element) => {
+      // Exclude elements inside the <footer> tag
+      $(element)
+        .find('h1, h2, h3, h4, p')
+        .each((index, el) => {
+          const elementType = $(el).prop('tagName').toLowerCase();
+          const content = $(el).text().trim().replace(/\s+/g, ' ');
+
+          // Check if content is already extracted
+          if (!extractedContent.has(content)) {
+            divs.push({ type: elementType, content });
+            extractedContent.add(content);
+          }
+        });
+    });
+
+    const extractedText = divs.map((div) => div.content).join('\n');
+    return extractedText;
+  } catch (error) {
+    console.error('Error:', error.message);
+    throw error;
+  }
+};
+
+const generatePrompt = (content) => {
+  const prompt = `Generate an ad headline and copy for a Google Ads campaign. Based on the provided content.
+  
+  Text: "${content}"
+  `;
+
+  return prompt;
+};
+
+const generateHeadlines = async (siteContent) => {
+  try {
+    if (!configuration.apiKey) {
+      throw new Error(
+        'OpenAI API key not configured, please follow instructions in README.md'
+      );
+    }
+
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: generatePrompt(siteContent),
+      temperature: 0.6,
+      max_tokens: 100,
+      n: numberOfChoices,
+    });
+
+    // Process the completion response here
+    const generatedChoices = completion.data.choices;
+    return generatedChoices;
+  } catch (error) {
+    console.error('Error with OpenAI API request:', error.message);
+    throw error;
+  }
+};
+
+const contentGeneration = async () => {
+  try {
+    const websiteUrl = 'https://www.sgsmiles.com/';
+    const extractedText = await getHtml(websiteUrl);
+    const generatedChoices = await generateHeadlines(extractedText);
+
+    const adContent = generatedChoices.map((choice) => {
+      const text = choice.text.trim();
+
+      // Extracting headline and copy from the generated text
+      const [headline, copy] = text.split('Copy:');
+
+      // Creating an object with the extracted headline and copy
+      return {
+        headline: headline.trim(),
+        copy: copy.trim(),
+      };
+    });
+
+    return adContent;
+  } catch (error) {
+    console.error('Error with content generation:', error.message);
+    return []; // Return an empty array in case of an error
+  }
+};
+
+contentGeneration()
+  .then((adContent) => {
+    console.log(adContent);
+  })
+  .catch((error) => {
+    console.error('Error with content generation:', error.message);
+  });
